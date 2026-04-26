7d054f81d13a772b280b462c09fde6dfd89e0c74
Evelyn333444 <brackesb12@gmail.com>
2026-04-25 20:38:11 -0400

    first push onboarding

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,43 +1,70 @@
 import React from "react";
-import NFT from "../../images/nft.png";
-import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
 
-const Landing = () => {
+const BrowseByCategory = () => {
   return (
-    <section
-      id="section-hero"
-      aria-label="section"
-      className="no-top no-bottom vh-100"
-      data-bgimage="url(images/bg-shape-1.jpg) bottom"
-      style={{ background: `url(${backgroundImage}) bottom / cover` }}
-    >
-      <div className="v-center">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-md-6">
-              <div className="spacer-single"></div>
-              <h6>
-                <span className="text-uppercase id-color-2">
-                  Ultraverse Market
-                </span>
-              </h6>
-              <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
-                Unit of data stored on a digital ledger, called a blockchain,
-                that certifies a digital asset to be unique and therefore not
-                interchangeable
-              </p>
-              <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
-                Explore
-              </Link>
-              <div className="mb-sm-30"></div>
+    <section id="section-category" className="no-top">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="text-center">
+              <h2>Browse by category</h2>
+              <div className="small-border bg-color-2"></div>
             </div>
-            <div className="col-md-6 xs-hide">
-              <img src={NFT} className="lazy img-fluid" alt="" />
-            </div>
+          </div>
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30 wow fadeInRight"
+            data-wow-delay=".1s"
+          >
+            <Link to="/explore" className="icon-box style-2 rounded">
+              <i className="fa fa-image"></i>
+              <span>Art</span>
+            </Link>
+          </div>
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30 wow fadeInRight"
+            data-wow-delay=".2s"
+          >
+            <Link to="/explore" className="icon-box style-2 rounded">
+              <i className="fa fa-music"></i>
+              <span>Music</span>
+            </Link>
+          </div>
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30 wow fadeInRight"
+            data-wow-delay=".3s"
+          >
+            <Link to="/explore" className="icon-box style-2 rounded">
+              <i className="fa fa-search"></i>
+              <span>Domain Names</span>
+            </Link>
+          </div>
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30 wow fadeInRight"
+            data-wow-delay=".4s"
+          >
+            <Link to="/explore" className="icon-box style-2 rounded">
+              <i className="fa fa-globe"></i>
+              <span>Virtual Worlds</span>
+            </Link>
+          </div>
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30 wow fadeInRight"
+            data-wow-delay=".5s"
+          >
+            <Link to="/explore" className="icon-box style-2 rounded">
+              <i className="fa fa-vcard"></i>
+              <span>Trading Cards</span>
+            </Link>
+          </div>
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30 wow fadeInRight"
+            data-wow-delay=".6s"
+          >
+            <Link to="/explore" className="icon-box style-2 rounded">
+              <i className="fa fa-th"></i>
+              <span>Collectibles</span>
+            </Link>
           </div>
         </div>
       </div>
@@ -45,4 +72,4 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+export default BrowseByCategory;
